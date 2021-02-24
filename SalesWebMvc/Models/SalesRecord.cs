bfdd9ff83d7fa5676c05f09fc3629d21a072e88c
Otavio <luiz_otavio71@hotmail.com>
2021-02-24 11:02:00 -0300

    implementing group serch finnaly project

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -7,8 +7,9 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -34,5 +34,24 @@ namespace SalesWebMvc.Services
                 .ToListAsync();
 
         }
+        public async Task<List<IGrouping<Department,SalesRecord>>> FindByDateGroupingAsync(DateTime? mindate,DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if(mindate.HasValue)
+            {
+                result = result.Where(x => x.Date >= mindate.Value);
+            }
+            if(maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x =>x.Seller.Department)
+                .ToListAsync();
+
+        }
     }
 }
